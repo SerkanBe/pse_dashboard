7822c9b4bb6d2b5e0c58a0c3c3ea3d949d4a2863
Serkan Bekdemir <pse@serkan.bekdemir.de>
2017-01-11 12:47:50 +0100

    add kartograph.min.js and a second map to index.php

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
 	<link href="/css/nouislider.min.css" rel="stylesheet">
 	
 	<!-- Kartograph Map -->
-	<script src="jquery.min.js"></script>
-    <script src="raphael-min.js"></script>
-    <script src="kartograph.js"></script>
+	<script src="../vendors/jquery/dist/jquery.min.js"></script>
+    <script src="../vendors/raphael/raphael.min.js"></script>
+    <script src="../vendors/kartograph/kartograph.min.js"></script>
     <!-- Bootstrap -->
     <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Font Awesome -->
@@ -494,5 +494,51 @@
 		  });			     
 	  </script>
 	  <!-- echart-linechart -->	  
+	  
+		<!-- kartograph-map -->
+	  <script>
+	  
+			  var svgUrl = 'http://kartograph.org/showcase/usa-projection/usa.svg',
+			opts = { };
+
+		kartograph.map('#usa_map2').loadMap(svgUrl, mapLoaded, opts);
+
+		function mapLoaded(map) {
+			map.addLayer('layer0', {
+				styles: {
+					stroke: '#aaa',
+					fill: '#f6f4f2'
+				},
+				mouseenter: function(d, path) {
+					path.attr('fill', Math.random() < 0.5 ? '#c04' : '#04c');
+				},
+				mouseleave: function(d, path) {
+					path.animate({ fill: '#f6f4f2' }, 1000);
+				}
+			});
+			
+			var points_of_interest = [
+				{ name: 'Juneau, AK', lat: 58.3, lon: -134.416667 },
+				{ name: 'Honolulu, HI', lat: 21.3, lon: -157.816667 },
+				{ name: 'San Francisco, CA', lat: 37.783333, lon: -122.416667 }
+			];
+
+			map.addSymbols({
+				type: kartograph.LabeledBubble,
+				data: points_of_interest,
+				location: function(d) { return [d.lon, d.lat] },
+				title: function(d) { return d.name; },
+				radius: 3,
+				center: false,
+				attrs: { fill: 'black' },
+				labelattrs: { 'font-size': 11 },
+				buffer: false
+		   });
+		}
+
+
+
+		</script>
+		<!-- /kartograph-map -->
   </body>
 </html>
